f7489a12eefa3ce18a1015f075f05e001afdda4b
Noel Bansikah <tandapnoelbansikah@gmail.com>
2026-07-27 16:39:27 +0100

    Create Arrays.swift

---
+++ basics/Arrays.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+//arrays play
+let workingDays: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+print(workingDays[2])
+
+for day in workingDays{
+  print(day)
+}
